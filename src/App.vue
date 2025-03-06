165e0965d042f9e1aa588a1fe335ac1126d8fb15
High-Petrushka <daniilprokopovic12@gmail.com>
2025-03-06 21:01:16 +0300

    Initial project point

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HeaderMenu from './components/menu/HeaderMenu.vue';
+import FooterMenu from './components/menu/FooterMenu.vue';
+</script>
+
+<template>
+<header></header>
+
+<main></main>
+
+<footer></footer>
+</template>
+
+<style scoped>
+
+</style>

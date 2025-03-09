9f48b379f61db4a0d5430b1ddec44f8f3a7c6c49
High-Petrushka <daniilprokopovic12@gmail.com>
2025-03-09 23:24:42 +0300

    Making a header menu pattern

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,24 @@ import HeaderMenu from './components/menu/HeaderMenu.vue';
 import FooterMenu from './components/menu/FooterMenu.vue';
 
 import Pattern from './components/Pattern.vue';
+
+import { reactive } from 'vue';
+
+const local = reactive({
+    page: [0],
+});
+
+function pageChanger(page) {
+    local.page[0] = page;
+    console.log(page);
+}
 </script>
 
 <template>
-<header></header>
+<header>
+    <HeaderMenu 
+    @change-page="pageChanger"/>
+</header>
 
 <main>
     <Pattern />
--- src/components/menu/HeaderMenu.vue
+++ src/components/menu/HeaderMenu.vue
@@ -1,5 +1,156 @@
-<script setup></script>
+<script setup>
+const emit = defineEmits([
+    'changePage',
+]);
+</script>
 
-<template></template>
+<template>
+    <div class="menu__cont">
+        <div class="menu">
+            <div class="logo__cont">
+                <a
+                class="logo"
+                @click.prevent="$emit('changePage', 0)">V'oS</a>
+            </div>
+            <div class="link__bar">
+                <div class="menu__link">
+                    <a
+                    href="#"
+                    @click.prevent="$emit('changePage', 1)">Headphones</a>
+                </div>
+                <div class="menu__link">
+                    <a
+                    href="#"
+                    @click.prevent="$emit('changePage', 2)">Speakers</a>
+                </div>
+                <div class="menu__link">
+                    <a
+                    href="#"
+                    @click.prevent="$emit('changePage', 3)">Accessories</a>
+                </div>
+                <div class="menu__link">
+                    <a
+                    href="#"
+                    @click.prevent="$emit('changePage', 4)">Vinyl</a>
+                </div>
+            </div>
+            <div class="menu__el">
+                <div class="menu__item">
+                    <img src="../../assets/icons/user.png" alt="User icon"
+                    @click="$emit('changePage', 5)">
+                </div>
+                <div class="menu__item">
+                    <img src="../../assets/icons/online-shopping.png" alt="Cart icon"
+                    @click="$emit('changePage', 6)">
+                </div>
+                <div class="menu__item" id="hamburger">
+                    <img src="../../assets/icons/hamburger.png" alt="hamburger menu icon">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+.menu {
+    width: 100%;
+    height: 8.4rem;
+
+    padding: 0 clamp(2rem, 0.544rem + 3.883vw, 8rem);
+    background: var(--background-color);
+    box-shadow: inset 0 -1px 0 0 var(--card-color);
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    position: fixed;
+}
+
+.logo {
+    text-decoration: none;
+
+    font-size: 2.8rem;
+    font-weight: 500;
+    line-height: 1.2;
+    letter-spacing: .2rem;
+
+    cursor: pointer;
+}
+
+.link__bar {
+    width: fit-content;
+    height: fit-content;
+
+    display: none;
+    gap: 4rem;
+}
+
+.menu__link {
+    width: fit-content;
+    height: fit-content;
+
+    cursor: pointer;
+
+    position: relative;
+}
+
+.menu__link a {
+    text-decoration: none;
+
+    font-size: 1.8rem;
+    font-weight: 400;
+    line-height: 1.3;
+    letter-spacing: -0.02rem;
+    color: var(--font-color);
+}
+
+.menu__link a::before {
+    content: '';
+
+    width: 0%;
+    height: 2px;
+    background: var(--font-color);
+
+    position: absolute;
+    bottom: 0;
+
+    transition: 0.35s;
+}
+
+.menu__link a:hover::before {
+    width: 100%;
+}
+
+.menu__el {
+    width: fit-content;
+    height: fit-content;
+
+    display: flex;
+    gap: 2rem;
+}
+
+.menu__item {
+    width: fit-content;
+    height: fit-content;
+
+    display: block;
+
+    cursor: pointer;
+}
+
+.menu__item img {
+    width: 2.2rem;
+    height: 2.2rem;
+}
+
+@media screen and (width > 1024px) {
+    #hamburger {
+        display: none;
+    }
+
+    .link__bar {
+        display: flex;
+    }
+}
+</style>

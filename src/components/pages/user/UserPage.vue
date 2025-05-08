6ae9160cb6f74cf59379cc2cf87cb9fdae5926b5
High-Petrushka <daniilprokopovic12@gmail.com>
2025-05-08 23:27:26 +0300

    Making a user menu styles

--- src/components/pages/user/UserPage.vue
+++ src/components/pages/user/UserPage.vue
@@ -1,9 +1,36 @@
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, onUpdated, reactive } from "vue";
 import { RouterLink, RouterView } from "vue-router";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
+
+const local = reactive({
+  actPage: {
+    profile: 1,
+    order: 0,
+    purchase: 0,
+  },
+});
+
+function changeActivePage(page) {
+  for (let i in local.actPage) {
+    if (i == page) {
+      local.actPage[i] = 1;
+    } else {
+      local.actPage[i] = 0;
+    }
+  }
+}
+
+onUpdated(() => {
+  const path = router.currentRoute.value.path;
+  const param = router.currentRoute.value.params.name;
+
+  const page = path.slice(7 + param.length);
+
+  changeActivePage(page);
+});
 
 onMounted(() => {
   router.push({ name: "Info" });
@@ -13,9 +40,25 @@ onMounted(() => {
 <template>
   <div class="cont">
     <div class="user__menu">
-      <RouterLink :to="{ name: 'Info' }">Profile</RouterLink>
-      <RouterLink :to="{ name: 'Order' }">Order</RouterLink>
-      <RouterLink :to="{ name: 'Purchase' }">Purchase</RouterLink>
+      <RouterLink
+        class="user__link"
+        :class="{ active__link: local.actPage.profile }"
+        :to="{ name: 'Info' }"
+        >Profile
+      </RouterLink>
+      <RouterLink
+        class="user__link"
+        :class="{ active__link: local.actPage.order }"
+        :to="{ name: 'Order' }"
+        >Order
+      </RouterLink>
+      <RouterLink
+        class="user__link"
+        :class="{ active__link: local.actPage.purchase }"
+        :to="{ name: 'Purchase' }"
+      >
+        Purchase
+      </RouterLink>
     </div>
   </div>
   <div class="cont">
@@ -28,8 +71,45 @@ onMounted(() => {
   width: 100%;
   height: fit-content;
 
+  padding: 3.6rem 0rem 0rem;
+
   display: flex;
   align-items: center;
   justify-content: space-between;
+}
+
+.main__cont {
+  gap: 0rem;
+}
+
+.user__link {
+  text-decoration: none;
+
+  font-size: clamp(1.6rem, 1.138rem + 1.233vw, 2.4rem);
+  color: var(--user-link-color);
+
+  transition: color 0.35s ease;
+}
+
+.user__link:hover {
+  color: var(--font-color);
+}
+
+.active__link {
+  color: var(--font-color);
+}
+
+@media screen and (width > 1024px) {
+  .main__cont {
+    gap: 0rem;
+  }
+
+  .user__menu {
+    padding: 5.2rem 0rem 0rem;
+  }
+
+  .user__link {
+    font-size: 24px;
+  }
 }
 </style>
